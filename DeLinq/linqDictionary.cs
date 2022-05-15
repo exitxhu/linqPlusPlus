42aae0a0bb812539b8505357f4fb7d210e006cd2
mohammadhadi <mohammadHadi@smartec.ir>
2022-05-15 15:57:11 +0430

    Add project files.

---
+++ DeLinq/linqDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqPlusPlus
+{
+    public static class linqDictionary
+    {
+        public static Dictionary<string, object> ToDictionary(this object source)
+        {
+            if (source == null)
+                ThrowExceptionWhenSourceArgumentIsNull();
+
+            var dictionary = new Dictionary<string, object>();
+            foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(source))
+                AddPropertyToDictionary<object>(property, source, dictionary);
+            return dictionary;
+        }
+
+        private static void AddPropertyToDictionary<T>(PropertyDescriptor property, object source, Dictionary<string, object> dictionary)
+        {
+            object value = property.GetValue(source);
+            dictionary.Add(property.Name, value);
+        }
+
+
+        private static void ThrowExceptionWhenSourceArgumentIsNull()
+        {
+            throw new ArgumentNullException("source", "Unable to convert object to a dictionary. The source object is null.");
+        }
+    }
+}
