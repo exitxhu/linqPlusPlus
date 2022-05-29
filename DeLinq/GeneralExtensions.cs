99647518392a2439e6589cccf9dd42d99608b214
mohammadhadi <mohammadHadi@smartec.ir>
2022-05-29 14:54:01 +0430

    has content

---
+++ DeLinq/GeneralExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqPlusPlus
+{
+    public static class GeneralExtensions
+    {
+        public static bool HasContent(this string str) => !(string.IsNullOrEmpty(str) && string.IsNullOrWhiteSpace(str));
+    }
+}
