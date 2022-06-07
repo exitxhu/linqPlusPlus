7ea6eaf5bee43b6be6221eab9e21ffdf2f4ab90d
mohammadhadi <mohammadHadi@smartec.ir>
2022-06-07 14:57:08 +0430

    expression friendly Ifnull

--- DeLinq/GeneralExtensions.cs
+++ DeLinq/GeneralExtensions.cs
@@ -9,5 +9,10 @@ namespace linqPlusPlus
     public static class GeneralExtensions
     {
         public static bool HasContent(this string str) => !(string.IsNullOrEmpty(str) && string.IsNullOrWhiteSpace(str));
+        /// <summary>
+        /// expression friendly boolean null propagation
+        /// </summary>
+        public static bool IfNull(this object obj, bool nullPart = true, bool notNullPart = true) => obj is null ? nullPart : notNullPart;
+
     }
 }
